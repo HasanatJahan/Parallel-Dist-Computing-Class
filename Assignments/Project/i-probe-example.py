58f9f2a248dc783fcc74ff6694c760ee0a223a35
Hasanat Jahan <jahanhasanat@gmail.com>
2022-05-10 18:41:11 -0400

    it is finding the minimum tour

--- Assignments/Project/i-probe-example.py
+++ Assignments/Project/i-probe-example.py
@@ -29,6 +29,7 @@ snd_buf = np.array(global_best_tour, dtype=np.intc)
 
 
 global_best_tour = random.randrange(100)
+print(f"value of global_tour {global_best_tour}")
 
 for dest in range(0, comm_size):
      if dest != rank:
@@ -41,8 +42,16 @@ while comm_world.Iprobe(MPI.ANY_SOURCE, tag = 11):
      received_cost = comm_world.recv(source=MPI.ANY_SOURCE, tag=11)
      if received_cost < global_best_tour:
           global_best_tour = received_cost
-     print(f"Global best tour {global_best_tour}")
      comm_world.Iprobe(MPI.ANY_SOURCE, tag = 11)
+
+# Now to gather the data 
+# create a struct to collect the data 
+# data_tuple = np.array((rank, global_best_tour), dtype='2i')
+rcv_buf = np.empty((), dtype=np.intc)
+new_snd_buf = np.array(global_best_tour, dtype=np.intc)
+comm_world.Allreduce(new_snd_buf, rcv_buf, op=MPI.MIN)
+
+print(f"My rank {rank} and then the min {rcv_buf}")
 
 
 
